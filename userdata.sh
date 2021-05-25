1cf05e6ef387954105ec1690b4cb5d3657aee80f
D DraFz <61958249+ddrafz-mbl@users.noreply.github.com>
2021-05-25 13:50:39 +0700

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 sudo apt-get update -y
+
 # Install OpenJDK 8
 sudo apt-get install openjdk-8-jdk -y
+
 # Install Jenkins
 sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo echo "deb https://pkg.jenkins.io/debian-stable binary/" >> /etc/apt/sources.list
 sudo apt-get update -y
 sudo apt-get install jenkins -y
+
+#install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-#install docker
+
 apt-get install docker-ce docker-ce-cli containerd.io -y
 systemctl enable docker
 systemctl start docker
